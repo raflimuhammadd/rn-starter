34f837e1a32da386dd621e46b831f88e16f4cc8e
raflimuhammadd <raflimuhh09@gmail.com>
2024-08-31 12:00:30 +0700

    working-with-content

--- src/screens/ComponentScreen.js
+++ src/screens/ComponentScreen.js
@@ -2,17 +2,22 @@ import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentScreen = () => {
+  const greeting = <Text style={styles.subTextStyle}>My Name is Rafli</Text>;
+
   return (
     <View>
-      <Text style={styles.textStyle}>This is Component Screen!</Text>
-      <Text>Hi There!</Text>
+      <Text style={styles.textStyle}>Getting started with react native!</Text>
+      {greeting}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   textStyle: {
-    fontSize: 30,
+    fontSize: 45,
+  },
+  subTextStyle: {
+    fontSize: 20,
   }
 })
 
